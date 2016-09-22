c3f04f29b297c31d33c3cd29d8963e616e5d3e7e
inerc <inerc@mail.ru>
2016-09-22 16:30:08 +0300

    React#Readux#03

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,21 +1,29 @@
 import path from 'path'
-
+import webpack from 'webpack';
 
 export default {
     devtools: 'eval-source-map',
     entry: [
+        'webpack-hot-middleware/client',
         path.join(__dirname, '/client/index.js')
     ],
     output: {
-        path: '/'
+        path: '/',
+        publicPath: '/'
     },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
 
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 include: path.join(__dirname, 'client'),
-                loaders: ['babel']
+                loaders: ['react-hot',  'babel']
             }
         ]
     },
