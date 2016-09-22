a8baba37e10919d3c456be637045c22cd61e980d
inerc <inerc@mail.ru>
2016-09-22 15:54:44 +0300

    React&Redux#02

---
+++ client/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by inerc on 21.09.16.
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
+
+render(<App />, document.getElementById('app'));
